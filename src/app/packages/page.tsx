56d1fae8c0c03ffa813d9e1ad4e2e8833bcdb2b7
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2026-01-20 13:04:02 +0000

    All Packages in moblie fit to 2 coloum

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -134,7 +134,7 @@ export default function PackagesPage() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
           {allPackages.length > 0 ? (
             allPackages.map((pkg) => (
             <Link href={pkg.href} key={pkg.title} className="block group">
