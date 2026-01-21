1dd243a81fd5100c686cbfb59cd9cc29cd6620ab
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2026-01-21 05:25:22 +0000

    change font to bueatiful

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from "next";
-import { Inter, Playfair_Display, Noto_Kufi_Arabic } from "next/font/google";
+import { Kanit, Noto_Kufi_Arabic } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { LangProvider } from "@/context/LangContext";
 
-const inter = Inter({
-  subsets: ["latin"],
+const kanit = Kanit({
+  subsets: ["latin", "thai"],
   weight: ["400", "500", "600", "700"],
-  variable: "--font-inter",
-});
-
-const playfair = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  variable: "--font-playfair",
+  variable: "--font-kanit",
 });
 
 const notoKufiArabic = Noto_Kufi_Arabic({
@@ -35,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="th" dir="ltr">
       <body
-        className={`${inter.variable} ${playfair.variable} ${notoKufiArabic.variable}`}
+        className={`${kanit.variable} ${notoKufiArabic.variable}`}
       >
         <LangProvider>
           <main>{children}</main>
