cc81b9f1aad01b887e344b0e70abce87c0fd02f9
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2026-01-21 01:30:54 +0000

    delete navigation bar above and below

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Kanit } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { LangProvider } from "@/context/LangContext";
-import { Header } from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
 
 const kanit = Kanit({ 
   subsets: ["thai", "latin"],
@@ -25,9 +23,7 @@ export default function RootLayout({
     <html lang="th">
       <body className={kanit.className}>
         <LangProvider>
-          <Header />
           <main>{children}</main>
-          <Footer />
         </LangProvider>
         <Toaster />
       </body>
