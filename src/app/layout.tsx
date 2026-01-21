76137a10018c9443ecb22a8cb2d4a722b721cd30
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2026-01-21 05:19:35 +0000

    change to Noto san Thai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import { Poppins, Playfair_Display, Noto_Sans_Thai, Noto_Kufi_Arabic } from "next/font/google";
+import { Noto_Sans_Thai, Noto_Kufi_Arabic } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { LangProvider } from "@/context/LangContext";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
-});
-
-const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-playfair-display",
-});
 
 const notoSansThai = Noto_Sans_Thai({ 
   subsets: ["thai", "latin"],
@@ -41,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="th" dir="ltr">
       <body
-        className={`${poppins.variable} ${playfairDisplay.variable} ${notoSansThai.variable} ${notoKufiArabic.variable}`}
+        className={`${notoSansThai.variable} ${notoKufiArabic.variable}`}
       >
         <LangProvider>
           <main>{children}</main>
