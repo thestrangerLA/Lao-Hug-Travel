86fa3f837dc3d7792886310e5949e28e95b7981e
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2026-01-24 14:33:54 +0000

    more length

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -407,7 +407,7 @@ export default function Home() {
                     <p className="text-2xl md:text-3xl font-bold text-primary-foreground">
                       {pageContent.heroSubTitle}
                     </p>
-                    <div className="w-32 h-1 bg-primary-foreground my-6"></div>
+                    <div className="w-48 h-1 bg-primary-foreground my-6"></div>
                     <h2 className="text-xl md:text-3xl font-semibold font-headline text-white">
                       {pageContent.heroTitle}
                     </h2>
